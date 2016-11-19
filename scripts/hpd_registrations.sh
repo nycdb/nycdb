fec804a4d3fe209397c41aa3521b0db6b56a6713
ziggy <zig@riseup.net>
2016-11-19 17:53:23 -0500

    hpd registrations script

--- scripts/hpd_registrations.sh
+++ scripts/hpd_registrations.sh
@@ -1,55 +1,59 @@
 #!/bin/bash
 
-source env.sh
+set -e
+source ./env.sh
+
+pwd=$(pwd)
+mkdir -p tmp/
 
 printf "***HPD Registrations***\n"
 
-cd $HPD_REPO_PATH
-mkdir -p tmp
+printf "Cleaning up the data\n"
+cat ${pwd}/data/hpd/Registration2016*.txt | python3 modules/hpd/data_cleanup.py 16 1> ${pwd}/tmp/registrations.txt 2> ${pwd}/tmp/registrations_errors.txt
+cat ${pwd}/data/hpd/RegistrationContact*.txt | python3 modules/hpd/data_cleanup.py 15 1> ${pwd}/tmp/contacts.txt 2> ${pwd}/tmp/contacts_errors.txt
 
-cat ${HPD_REGISTRATIONS_FILE} | python data_cleanup.py 16 1> tmp/registrations.txt 2> tmp/registrations_errors.txt
-cat ${HPD_CONTACTS_FILE} | python data_cleanup.py 15 1> tmp/contacts.txt 2> tmp/contacts_errors.txt
+printf "Removed "$(cat ${pwd}/tmp/registrations_errors.txt | wc -l)" bad lines from the registrations data\n"
+printf "Removed "$(cat ${pwd}/tmp/contacts_errors.txt | wc -l)" bad lines from the contacts data\n"
 
-printf "Removed "$(cat tmp/registrations_errors.txt | wc -l)" bad lines from the registrations data\n"
-printf "Removed "$(cat tmp/contacts_errors.txt | wc -l)" bad lines from the contacts data\n"
-
-HPD_REGISTRATIONS_FILE=$(pwd)/tmp/registrations.txt
-HPD_CONTACTS_FILE=$(pwd)/tmp/contacts.txt
-
-HPD_REGISTRATIONS_FILE=$(pwd)/tmp/registrations.txt
-HPD_CONTACTS_FILE=$(pwd)/tmp/contacts.txt
-
-printf 'create table and COPY data\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/schema.sql'
+printf 'Create table \n'
+execute_sql ${pwd}/modules/hpd/sql/schema.sql
 
 printf 'Inserting data\n'
-psql -d ${NYCDB_DATABASE} -c "COPY hpd.registrations FROM '"${HPD_REGISTRATIONS_FILE}"' (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
-psql -d ${NYCDB_DATABASE} -c "COPY hpd.contacts FROM '"${HPD_CONTACTS_FILE}"' (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
-psql -d ${NYCDB_DATABASE} -c "COPY hpd.bbl_lookup FROM '"${BBL_LAT_LNG}"' (FORMAT CSV,  HEADER TRUE);"
 
-printf 'cleanup contact addresses\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/address_cleanup.sql'
+HPD_REGISTRATIONS_FILE=${pwd}/tmp/registrations.txt
+HPD_CONTACTS_FILE=${pwd}/tmp/contacts.txt
+BBL_LAT_LNG=${pwd}/modules/hpd/bbl_lat_lng.txt
 
-printf 'cleanup registration addresses\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/registrations_clean_up.sql'
+cat $HPD_REGISTRATIONS_FILE | execute_sql_cmd "COPY hpd.registrations FROM STDIN (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
+cat $HPD_CONTACTS_FILE | execute_sql_cmd "COPY hpd.contacts FROM STDIN (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
+cat $BBL_LAT_LNG | execute_sql_cmd "COPY hpd.bbl_lookup FROM STDIN (FORMAT CSV,  HEADER TRUE);"
+
+printf 'Cleanup contact addresses\n'
+execute_sql ${pwd}/modules/hpd/sql/address_cleanup.sql
+
+printf 'Cleanup registration addresses\n'
+execute_sql ${pwd}/modules/hpd/sql/registrations_clean_up.sql
 
 printf 'Add function anyarray_uniq()\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/anyarray_uniq.sql'
+execute_sql ${pwd}/modules/hpd/sql/anyarray_uniq.sql
 
 printf 'Add function anyarray_remove_null\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/anyarray_remove_null.sql'
+execute_sql ${pwd}/modules/hpd/sql/anyarray_remove_null.sql
 
-printf 'Add aggregate functions first() and last()\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/first_last.sql'
+printf "Add aggregate functions first() and last()\n"
+execute_sql ${pwd}/modules/hpd/sql/first_last.sql
 
 printf 'Creating corporate_owners table\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/corporate_owners.sql'
+execute_sql ${pwd}/modules/hpd/sql/corporate_owners.sql
 
 printf 'Geocodes registrations via pluto\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/registrations_geocode.sql'
+execute_sql ${pwd}/modules/hpd/sql/registrations_geocode.sql
 
 printf 'Creating table registrations_grouped_by_bbl\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/registrations_grouped_by_bbl.sql'
+execute_sql ${pwd}/modules/hpd/sql/registrations_grouped_by_bbl.sql
 
 printf 'Indexing tables\n'
-psql -d ${NYCDB_DATABASE} -f 'sql/index.sql'
+execute_sql ${pwd}/modules/hpd/sql/index.sql
+
+printf 'Removing temporary files\n'
+rm -r ${pwd}/tmp
