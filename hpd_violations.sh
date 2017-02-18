70a6e145471a15e62f3bf05d41e1f7bd92ddfe48
ziggy <ziggy@elephant-bird.net>
2017-02-18 10:23:02 -0500

    add missing cd

--- hpd_violations.sh
+++ hpd_violations.sh
@@ -9,7 +9,7 @@ printf "***HPD Violations***\n"
 cp env.sh ${pwd}/modules/hpd-violations/pg_setup.sh
 echo "HPD_VIOLATIONS_DATA_FOLDER=${pwd}/data/hpd_violations/data" >> ${pwd}/modules/hpd-violations/pg_setup.sh
 
-${pwd}/modules/hpd-violations
+cd ${pwd}/modules/hpd-violations
 printf "Unzipping HPD Violations \n"
 ./unzip.sh
 printf "Inserting HPD Violations \n"
