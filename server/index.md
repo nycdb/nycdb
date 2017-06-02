829a8211a282d4e6188d9f17d84ad56edc82a225
ziggy <ziggy@elephant-bird.net>
2017-06-02 13:02:32 -0400

    the start of an api server configuration

---
+++ server/index.md
@@ -0,0 +1,6 @@
+# NYC-DB SERVER
+
+## ABOUT
+
+NYC-DB is an open source database of housing data for NYC. Built by and for community activists.
+
---
+++ server/user.sql
@@ -0,0 +1,9 @@
+create role anon;
+
+grant usage on schema public to anon;
+grant select on all tables in schema public to anon;
+-- grant execute on all functions in schema public to anon;
+
+
+
+
