de6bf1e9bf775b2f25e45e537efa21c426205690
ziggy <ziggy@elephant-bird.net>
2017-10-12 20:30:44 -0400

    update hpd data links

--- scripts/download.sh
+++ scripts/download.sh
@@ -65,7 +65,7 @@ dofsales () {
 hpd () {
     printf "Downloading hpd registration data\n"
     mkdir -p data/hpd
-    wget http://www1.nyc.gov/assets/hpd/downloads/misc/Registrations20170901.zip -O data/hpd/registrations.zip
+    wget http://www1.nyc.gov/assets/hpd/downloads/misc/Registrations20171001.zip -O data/hpd/registrations.zip
     printf "Unzipping hpd registration data\n"
     unzip data/hpd/registrations.zip -d data/hpd
     rm data/hpd/*.xml
