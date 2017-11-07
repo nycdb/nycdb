01594fd5903fcb0956bdfcc1dd15819019480a6e
ziggy <ziggy@elephant-bird.net>
2017-11-07 09:17:28 -0500

    add wait-for-pg script

---
+++ src/scripts/wait-for-pg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ATTEMPTS=0
+
+PG_READY='pg_isready -h 127.0.0.1 -p 7777 -U postgres --quiet'
+
+until $PG_READY; do
+    if [[ $ATTEMPTS -eq 10 ]]; then
+	exit 1
+    fi
+    ((ATTEMPTS++))
+    sleep 0.5
+done
