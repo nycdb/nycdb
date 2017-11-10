5a9cd90a8cbcdac9740ed3de6341b1c8735923cb
ziggy <ziggy@elephant-bird.net>
2017-11-10 17:30:58 -0500

    remove dashboard

--- dashboard/dashboard/toN.js
+++
@@ -1,8 +0,0 @@
-const isNumber = require('lodash/isNumber');
-const toNumber = require('lodash/toNumber');
-
-module.exports = (n) => {
-  return isNumber(n) ? n : toNumber(n.replace(',', '').replace('$', ''));
-};
-
-
