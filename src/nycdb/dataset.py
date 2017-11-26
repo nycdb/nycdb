c3c9b3e78e4515b85227e0974a683071a373e360
ziggy <ziggy@elephant-bird.net>
2017-11-25 20:07:11 -0500

    put class Datasets in it's own file

--- src/nycdb/dataset.py
+++ src/nycdb/dataset.py
@@ -99,23 +99,3 @@ class Dataset:
             self.db = Database(self.args, table_name=self.name)
 
 
-class Datasets:
-    """ All NYCDB datasets """
-    
-    def __init__(self, args):
-        self.args = args
-        self.datasets = [ Dataset(k, args=args) for k in datasets() ]
-
-    def download_all(self):
-        for d in self.datasets:
-            d.download_files()
-
-
-    def transform_all(self):
-        for d in self.datasets:
-            d.transfrom_files()
-            
-
-    def import_all(self):
-        for d in self.datasets:
-            d.db_import()
---
+++ src/nycdb/datasets.py
@@ -0,0 +1,21 @@
+from .dataset import Dataset
+
+class Datasets:
+    """ All NYCDB datasets """
+    
+    def __init__(self, args):
+        self.args = args
+        self.datasets = [ Dataset(k, args=args) for k in datasets() ]
+
+    def download_all(self):
+        for d in self.datasets:
+            d.download_files()
+
+    def transform_all(self):
+        for d in self.datasets:
+            d.transfrom_files()
+            
+
+    def import_all(self):
+        for d in self.datasets:
+            d.db_import()
