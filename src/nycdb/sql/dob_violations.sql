a4f4e1b4dffdf5fc73a384d5409ef6b36e55790f
0xstarcat <xxStarcat@gmail.com>
2018-12-10 19:17:23 -0500

    fix sql

--- src/nycdb/sql/dob_violations.sql
+++ src/nycdb/sql/dob_violations.sql
@@ -1,4 +1,4 @@
 CREATE INDEX dob_violations_bbl_idx on dob_violations (bbl);
 CREATE UNIQUE INDEX dob_violations_isndobbisviol_idx on dob_violations (isndobbisviol);
 CREATE INDEX dob_violations_violationtypecode on dob_violations (violationtypecode);
-CREATE UNIQUE INDEX dob_violations_ecb_number_idx on ecb_violations (ecbnumber);
+CREATE INDEX dob_violations_ecb_number_idx on dob_violations (ecbnumber);
