078b9e21bfdccc6f4c9f6f9c22a8580e8d62ebf5
ziggy <ziggy@elephant-bird.net>
2019-01-21 18:47:40 -0500

    index cols bbl, balancedue, violationdate on table oath_hearings (#70)

---
+++ src/nycdb/sql/oath_hearings.sql
@@ -0,0 +1,3 @@
+CREATE INDEX oath_hearings_bbl_idx on oath_hearings (bbl);
+CREATE INDEX oath_hearings_balance_due_idx on oath_hearings (balancedue);
+CREATE INDEX oath_hearings_violation_date_idx on oath_hearings (violationdate);
