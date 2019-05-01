6335e1d8bd76e886c7aa69159ea41efaa5e819a5
Atul Varma <varmaa@gmail.com>
2019-05-01 17:33:09 -0400

    Combine marshal evictions 2017 and 2018 into one dataset w/ two tables.

--- src/nycdb/verify.py
+++ src/nycdb/verify.py
@@ -40,7 +40,10 @@ TABLES = {
         'acris_property_type_codes': 46,
         'acris_ucc_collateral_codes': 8
     },
-    'marshal_evictions_17': {'marshal_evictions_17': 17000},
+    'marshal_evictions': {
+        'marshal_evictions_17': 17000,
+        'marshal_evictions_18': 19000
+    },
     'oath_hearings': {'oath_hearings': 10000000}
 }
 
