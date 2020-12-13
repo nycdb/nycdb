1717962cf9b597bab8f29a640d398f6a74375e3a
Stephen Suffian <stephen.suffian@gmail.com>
2020-12-12 23:30:56 -0500

    Fix pyproj deprecated call and reverse lat,lng order due to updated projection.

--- src/tests/unit/test_geo.py
+++ src/tests/unit/test_geo.py
@@ -3,5 +3,5 @@ import nycdb
 def test_ny_state_coords_to_lat_lng():
     coords = [987838, 195989]
     result = nycdb.geo.ny_state_coords_to_lat_lng(*coords)
-    expected = [-73.98706, 40.70462]
+    expected = [40.70462, -73.98706]
     assert list(map(lambda x: round(x, 5), result)) == expected
