1e468e67aa7806172ef89b27823228d820d42723
Sam Rabiyah <sraby@users.noreply.github.com>
2021-05-10 17:25:03 -0400

    remove unneeded index

--- src/nycdb/sql/hpd_litigations.sql
+++ src/nycdb/sql/hpd_litigations.sql
@@ -1,4 +1,3 @@
-CREATE UNIQUE INDEX hpd_litigations_vacateordernumber_idx on hpd_litigations (vacateordernumber);
 CREATE INDEX hpd_litigations_bbl_idx on hpd_litigations (bbl);
 CREATE INDEX hpd_litigations_bin_idx on hpd_litigations (bin);
 CREATE INDEX hpd_litigations_buildingid_idx on hpd_litigations (buildingid);
