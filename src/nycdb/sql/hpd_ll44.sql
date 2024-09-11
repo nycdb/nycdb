e293087f9dc4cd290060edb7521bd96d03ec070b
Maxwell Austensen <m.austensen@gmail.com>
2024-09-10 20:17:14 -0400

    remove sql from excluded table

--- src/nycdb/sql/hpd_ll44.sql
+++ src/nycdb/sql/hpd_ll44.sql
@@ -3,5 +3,3 @@ CREATE INDEX hpd_ll44_buildings_projectid_idx on hpd_ll44_buildings (projectid);
 CREATE INDEX hpd_ll44_buildings_buildingid_idx on hpd_ll44_buildings (buildingid);
 
 CREATE INDEX hpd_ll44_projects_projectid_idx on hpd_ll44_projects (projectid);
-
-CREATE INDEX hpd_ll44_disqualified_list_bbl_idx on hpd_ll44_disqualified_list (bbl);
