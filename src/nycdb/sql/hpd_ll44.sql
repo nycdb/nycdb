beb587fd646aa2aa2f226de4754ede971fb76ac8
Maxwell Austensen <m.austensen@gmail.com>
2024-10-16 09:51:25 -0400

    handle nulls in creating boroid

--- src/nycdb/sql/hpd_ll44.sql
+++ src/nycdb/sql/hpd_ll44.sql
@@ -1,5 +1,5 @@
 ALTER TABLE hpd_ll44_buildings ADD COLUMN boroid int;
-UPDATE hpd_ll44_buildings SET boroid = substr(bin, 1, 1)::int;
+UPDATE hpd_ll44_buildings SET boroid = nullif(substr(bin, 1, 1), '')::int;
 
 CREATE INDEX hpd_ll44_buildings_bbl_idx on hpd_ll44_buildings (bbl);
 CREATE INDEX hpd_ll44_buildings_projectid_idx on hpd_ll44_buildings (projectid);
