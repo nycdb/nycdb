cb399ed4f89d90d57cd5d61849fab447a3c86a19
Maxwell Austensen <m.austensen@gmail.com>
2024-10-16 09:36:00 -0400

    use raw data for bbl column, add boroid

--- src/nycdb/sql/hpd_ll44.sql
+++ src/nycdb/sql/hpd_ll44.sql
@@ -1,3 +1,6 @@
+ALTER TABLE hpd_ll44_buildings ADD COLUMN boroid int;
+UPDATE hpd_ll44_buildings SET boroid = substr(bin, 1, 1)::int;
+
 CREATE INDEX hpd_ll44_buildings_bbl_idx on hpd_ll44_buildings (bbl);
 CREATE INDEX hpd_ll44_buildings_projectid_idx on hpd_ll44_buildings (projectid);
 CREATE INDEX hpd_ll44_buildings_buildingid_idx on hpd_ll44_buildings (buildingid);
