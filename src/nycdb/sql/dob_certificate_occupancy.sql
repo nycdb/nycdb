3cee673e15f7ff0b15b3ef2a553b2cc92f5d5ae7
Maxwell Austensen <m.austensen@gmail.com>
2025-02-21 17:55:34 -0500

    Add DOB NOW table to existing certificate of occupancy dataset (#371)
    There is an additional table of CofO data that's managed under the newer DOB NOW system. This PR adds it as an additional table in the existing dob_certificate_occupancy dataset.
    
    Also, in the github action workflow for publishing to PyPi there is a package (upload-artifacts) that required a version update

--- src/nycdb/sql/dob_certificate_occupancy.sql
+++ src/nycdb/sql/dob_certificate_occupancy.sql
@@ -13,3 +13,12 @@ CREATE INDEX dob_foil_certificate_occupancy_bbl_idx on dob_foil_certificate_occu
 CREATE INDEX dob_foil_certificate_occupancy_bin_idx on dob_foil_certificate_occupancy (bin);
 CREATE INDEX dob_foil_certificate_occupancy_jobnumber_idx on dob_foil_certificate_occupancy (jobnumber);
 CREATE INDEX dob_foil_certificate_occupancy_issuedate_idx on dob_foil_certificate_occupancy (issuedate);
+
+
+ALTER TABLE dob_now_certificate_occupancy ADD COLUMN bbl char(10);
+UPDATE dob_now_certificate_occupancy SET bbl = substr(bin, 1, 1) || lpad(block::text, 5, '0') || lpad(lot::text, 4, '0');
+
+CREATE INDEX dob_now_certificate_occupancy_bbl_idx on dob_now_certificate_occupancy (bbl);
+CREATE INDEX dob_now_certificate_occupancy_bin_idx on dob_now_certificate_occupancy (bin);
+CREATE INDEX dob_now_certificate_occupancy_jobfilingname_idx on dob_now_certificate_occupancy (jobfilingname);
+CREATE INDEX dob_now_certificate_occupancy_cofoissuancedate_idx on dob_now_certificate_occupancy (cofoissuancedate);
