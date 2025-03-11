5471bd41fda4ea1e2eb01fbc3aefa618822a1cb4
Maxwell Austensen <m.austensen@gmail.com>
2025-03-11 11:01:49 -0400

    add static 25a edition of clipped boundaries dataset

--- src/tests/integration/test_nycdb.py
+++ src/tests/integration/test_nycdb.py
@@ -808,6 +808,24 @@ def test_boundaries(conn):
     assert has_one_row(conn, "select 1 where to_regclass('public.nyct2020_boroct2020_idx') is NOT NULL")
 
 
+def test_boundaries_25a(conn):
+    setup_postgis(conn)
+    boundaries_25a = nycdb.Dataset('boundaries_25a', args=ARGS)
+    boundaries_25a.drop()
+    boundaries_25a.db_import()
+
+    for schema in boundaries_25a.schemas:
+        table_name = schema["table_name"]
+        assert row_count(conn, table_name) == 5
+        assert get_srid(conn, table_name, 'geom') == 2263
+        assert has_one_row(conn, f"select 1 where to_regclass('public.{table_name}_geom_idx') is NOT NULL")
+
+    assert has_one_row(conn, "select 1 where to_regclass('public.nynta2010_25a_nta2010_idx') is NOT NULL")
+    assert has_one_row(conn, "select 1 where to_regclass('public.nynta2020_25a_nta2020_idx') is NOT NULL")
+    assert has_one_row(conn, "select 1 where to_regclass('public.nyct2010_25a_boroct2010_idx') is NOT NULL")
+    assert has_one_row(conn, "select 1 where to_regclass('public.nyct2020_25a_boroct2020_idx') is NOT NULL")
+
+
 def test_boundaries_water_included(conn):
     setup_postgis(conn)
     boundaries_water_included = nycdb.Dataset('boundaries_water_included', args=ARGS)
@@ -819,6 +837,7 @@ def test_boundaries_water_included(conn):
         assert row_count(conn, table_name) == 5
         assert get_srid(conn, table_name, 'geom') == 2263
         assert has_one_row(conn, f"select 1 where to_regclass('public.{table_name}_geom_idx') is NOT NULL")
+
 
 def test_boundaries_water_included_25a(conn):
     setup_postgis(conn)
