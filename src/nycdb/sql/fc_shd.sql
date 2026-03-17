fd8e9ad6e8713d4adbeee40bb2cc33fe32208c3b
Maxwell Austensen <m.austensen@gmail.com>
2026-03-17 16:36:16 -0400

    sql typo

--- src/nycdb/sql/fc_shd.sql
+++ src/nycdb/sql/fc_shd.sql
@@ -1,4 +1,4 @@
 CREATE INDEX fc_shd_building_bbl_idx on fc_shd_building (bbl);
 
-ALTER TABLE fc_shd_subsidy RENAME COLUMN ref_bbl to bbl;
+ALTER TABLE fc_shd_subsidy RENAME COLUMN refbbl to bbl;
 CREATE INDEX fc_shd_subsidy_bbl_idx on fc_shd_subsidy (bbl);
