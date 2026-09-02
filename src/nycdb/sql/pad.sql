c5bd17eab43c8d23bd170a421eb46f3cd8a2afa2
Maxwell Austensen <m.austensen@gmail.com>
2026-09-02 12:44:38 -0400

    update indexes for PAD addresses

--- src/nycdb/sql/pad.sql
+++ src/nycdb/sql/pad.sql
@@ -1,2 +1,5 @@
-CREATE INDEX on pad_adr (bbl);
-CREATE INDEX on pad_adr (bin);
+CREATE INDEX ON pad_adr (bin);
+
+CREATE INDEX ON pad_adr (bbl, lhnd, stname)
+WHERE NULLIF(TRIM(lhnd), '') IS NOT NULL
+  AND NULLIF(TRIM(stname), '') IS NOT NULL;
